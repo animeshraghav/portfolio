d358ee8ec08c1bbff811b560a43e426776126e05
Animesh Raghav <95857840+animeshraghav@users.noreply.github.com>
2023-12-23 23:21:21 +0530

    updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,12 +9,11 @@ export const Bio = {
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/animeshraghav",
-    resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-    linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-    twitter: "https://twitter.com/RishavChanda",
-    insta: "https://www.instagram.com/rishav_chanda/",
-    facebook: "https://www.facebook.com/rishav.chanda.165/",
+    resume: "https://drive.google.com/file/d/1NUXSeDSv3jKlSSVshJhC0yrDWzp9QmRl/view?usp=sharing",
+    linkedin: "www.linkedin.com/in/animesh-raghav",
+    // twitter: 
+    insta: "https://www.instagram.com/animesh_raghav/",
+    // facebook: 
   };
   
   export const skills = [
@@ -288,30 +287,30 @@ export const Bio = {
   export const education = [
     {
       id: 0,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
-      school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
-      date: "Oct 2021 - Sep 2025",
-      grade: "8.71 CGPA",
-      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
-      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+      img: "/images/vit.jpg",
+      school: "Vellore Institute of Technology, Bhopal",
+      date: "Oct 2020 - Sep 2025",
+      grade: "8.45 CGPA",
+      desc: "I am currently pursuing Integrated Master's degree in Artificial Intelligence and Machine Learning at VIT, Bhopal . I have completed 8 semesters and have a CGPA of 8.45. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Photography club at VIT, where I am learning and working with the talented team to showcase the photography skills in vision to promote university",
+      degree: "Integrated Masters of Technology - Int. M.Tech (Artificial Intelligence and Machine Learning)",
     },
     {
       id: 1,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-      school: "Methodist School, Dankuni",
-      date: "Apr 2019 - Apr 2025",
-      grade: "88.2%",
-      desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
-      degree: "ISC(XII), Science with Computer",
+      img: "/images/Sanskar.jpeg",
+      school: "Sanskar The Co-Educational School",
+      date: "April 2019",
+      grade: "75%",
+      desc: "I completed my class 12 high school education at Sanskar The Co-Educational School, where I studied Science with Computer Science.",
+      // degree: "ISC(XII), Science with Computer",
     },
     {
       id: 2,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-      school: "Methodist School, Dankuni",
-      date: "Apr 2017 - Apr 2019",
-      grade: "92.3%",
-      desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
-      degree: "ICSC(X), Science with Computer",
+      img: "/images/Sanskar.jpeg",
+      school: "Sanskar The Co-Educational School",
+      date: "April 2017",
+      grade: "9.6/10",
+      desc: "I completed my class 10 education at Sanskar The Co-Educational School, where I studied Science with Computer Application.",
+      // degree: "ICSC(X), Science with Computer",
     },
   ];
   
